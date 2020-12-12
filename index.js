527e8b980b386cb41d9508431905de6c94386f8b
won <rjsdnql123@gmail.com>
2020-12-13 02:22:33 +0900

    initCommit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+const PORT = process.env.PORT;
+const pickRouter = require('./routes/realState');
+
+function server () {
+    console.log(PORT)
+}
+app.use('/realstate', pickRouter)
+
+app.use('/',(req,res) => {
+    res.json({message: 'ServerOn'})
+})
+
+app.listen(PORT,server)
